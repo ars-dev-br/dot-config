1dc410f585086eb4cd7e566e2594cbdfd3b03ef9
Andre Ramaciotti <github@ars.dev.br>
2025-01-24 14:54:01 -0300

    Add function to split windows in a four by two grid

--- emacs/init.el
+++ emacs/init.el
@@ -486,6 +486,32 @@ current frame in a counterclockwise direction."
     (set-window-buffer sixth-window sixth-buffer))
   (balance-windows))
 
+(defun ars-window--split-window-four-by-two-grid ()
+  "Set the current frame layout to a three-by-two grid."
+  (interactive)
+  (delete-other-windows)
+  (let* ((previous-files (ars-window--previous-files))
+	 (second-buffer (or (nth 0 previous-files) (current-buffer)))
+	 (third-buffer (or (nth 1 previous-files) (current-buffer)))
+	 (fourth-buffer (or (nth 2 previous-files) (current-buffer)))
+         (fifth-buffer (or (nth 3 previous-files) (current-buffer)))
+         (sixth-buffer (or (nth 4 previous-files) (current-buffer)))
+         (seventh-buffer (or (nth 5 previous-files) (current-buffer)))
+         (eighth-buffer (or (nth 6 previous-files) (current-buffer)))
+	 (second-window (split-window-right))
+         (third-window (split-window-right))
+         (fourth-window (split-window-right))
+         (fifth-window (split-window-below))
+         (sixth-window (split-window second-window nil 'below))
+         (seventh-window (split-window third-window nil 'below))
+         (eighth-window (split-window fourth-window nil 'below)))
+    (set-window-buffer second-window second-buffer)
+    (set-window-buffer third-window third-buffer)
+    (set-window-buffer fourth-window fourth-buffer)
+    (set-window-buffer fifth-window fifth-buffer)
+    (set-window-buffer sixth-window sixth-buffer))
+  (balance-windows))
+
 (defun ars-window--switch-to-previous-file ()
   "Switch to the most recently used file."
   (interactive)
@@ -520,5 +546,6 @@ current frame in a counterclockwise direction."
 
 (global-set-key (kbd "C-c w x") 'ars-window--split-window-two-by-two-grid)
 (global-set-key (kbd "C-c w 6") 'ars-window--split-window-three-by-two-grid)
+(global-set-key (kbd "C-c w 8") 'ars-window--split-window-four-by-two-grid)
 
 (global-set-key (kbd "C-c b k") 'ars-window--kill-all-buffers)
