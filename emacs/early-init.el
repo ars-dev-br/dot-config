66c91fd23cfe2f706b0f9acaf45a4174838bc87f
Andre Ramaciotti <github@ars.dev.br>
2026-06-18 13:58:01 -0300

    Improve GC thresholds

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -2,6 +2,10 @@
 
 (setq package-enable-at-startup nil)
 
-(setq gc-cons-threshold (* 20 1024 1024)) ;; initial threshold
-(add-hook 'emacs-startup-hook ;; threshold after init
-          (lambda () (setq gc-cons-threshold (* 128 1024 1024))))
+(setq gc-cons-threshold (* 128 1024 1024)
+      gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 20 1024 1024)
+                  gc-cons-percentage 0.1)))
