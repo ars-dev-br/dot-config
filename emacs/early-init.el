c19fa4e0456c8733ea38be0416f24b308887af3b
Andre Ramaciotti <github@ars.dev.br>
2026-06-18 13:58:32 -0300

    Silence native compilation errors

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+(setq native-comp-async-report-warnings-errors 'silent)
 
 (setq gc-cons-threshold (* 128 1024 1024)
       gc-cons-percentage 0.6)
